df5d02c57e167fef124a3f063e830d956c5898ac
Raimo Radczewski <raimoradczewski@googlemail.com>
2017-10-22 00:15:02 +0200

    Add magicline to CLI

--- dist/cli.js
+++ dist/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
--- rollup.config.js
+++ rollup.config.js
@@ -17,9 +17,7 @@ export default [
   },
   {
     input: 'src/cli.js',
-    output: [
-      { file: pkg.bin, format: 'cjs' }
-    ],
+    output: [{ file: pkg.bin, format: 'cjs', banner: '#!/usr/bin/env node' }],
     external: ['fs', 'yargs', 'prettier'],
     plugins: [
       babel({
